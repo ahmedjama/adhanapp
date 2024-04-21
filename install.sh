61263800fc6e8e77257fe7db9c28c7c252af7b17
Ahmed Jama <github@ahmedjama.com>
2024-04-21 20:07:38 +0100

    update install script

--- install.sh
+++ install.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Define variables
-SERVICE_NAME="adhanapp"
-USERNAME=$(whoami)
-BINARY_URL="https://github.com/ahmedjama/adhanapp/releases/download/v1.0.0/adhanapp-x86_64-unknown-linux-gnu"
-SERVICE_PATH="$HOME/.config/systemd/user/$SERVICE_NAME.service"
-
 # Check if root
 if [[ $EUID -eq 0 ]]; then
     echo "This script should be run as a non-root user."
@@ -45,19 +39,23 @@ echo "$api_content" | jq . > "$api_file"
 echo "File '$api_file' created successfully!"
 
 # Download the binary
+binary_url="https://github.com/ahmedjama/adhanapp/releases/download/v1.0.0/adhanapp-x86_64-unknown-linux-gnu"
 binary_file="$HOME/bin/adhanapp"
 
 # Create bin directory if it doesn't exist
 mkdir -p "$HOME/bin"
 
-# Download the binary to ~/bin and make it executable
-curl -L -o "$binary_file" "$BINARY_URL"
-chmod +x "$binary_file"
-echo "Binary downloaded to '$binary_file'."
+# Download the binary to ~/bin and make it executable if it doesn't exist
+if [ ! -f "$binary_file" ]; then
+  curl -L -o "$binary_file" "$binary_url"
+  chmod +x "$binary_file"
+  echo "Binary downloaded to '$binary_file'."
+else
+  echo "Binary '$binary_file' already exists. Skipping download."
+fi
 
 # Create the systemd service file
-mkdir -p "$HOME/.config/systemd/user/"
-cat > "$SERVICE_PATH" <<EOF
+cat > "$HOME/.config/systemd/user/adhanapp.service" <<EOF
 [Unit]
 Description=AdhanApp Service
 After=network.target
@@ -73,7 +71,27 @@ EOF
 
 # Reload systemd user units and start the service
 systemctl --user daemon-reload
-systemctl --user enable "$SERVICE_NAME"
-systemctl --user start "$SERVICE_NAME"
+systemctl --user enable adhanapp
+systemctl --user start adhanapp
 
 echo "adhanapp installed and started successfully."
+
+# Download the Fajr adhan MP3 file if it doesn't exist
+fajr_mp3_path="$HOME/adhanapp/media/fajr/adhan-fajr.mp3"
+if [ ! -f "$fajr_mp3_path" ]; then
+  cd "$HOME/adhanapp/media/fajr" || exit
+  wget https://github.com/ahmedjama/adhanapp/raw/main/media/fajr/adhan-fajr.mp3
+  echo "Fajr adhan MP3 downloaded to '$fajr_mp3_path'."
+else
+  echo "Fajr adhan MP3 already exists. Skipping download."
+fi
+
+# Download the other adhan MP3 file if it doesn't exist
+other_mp3_path="$HOME/adhanapp/media/other/adhan-makkah2-dua.mp3"
+if [ ! -f "$other_mp3_path" ]; then
+  cd "$HOME/adhanapp/media/other" || exit
+  wget https://github.com/ahmedjama/adhanapp/raw/main/media/other/adhan-makkah2-dua.mp3
+  echo "Other adhan MP3 downloaded to '$other_mp3_path'."
+else
+  echo "Other adhan MP3 already exists. Skipping download."
+fi
